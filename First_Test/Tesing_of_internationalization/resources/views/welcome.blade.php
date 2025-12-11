111ddb404ce5bc016ae10fbb1f2be58fcd9acc6b
Piraat-Bram <bramkerste125@gmail.com>
2025-12-11 18:15:51 +0100

    More additions

--- First_Test/Tesing_of_internationalization/resources/views/welcome.blade.php
+++ First_Test/Tesing_of_internationalization/resources/views/welcome.blade.php
@@ -3,6 +3,10 @@
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             @include('partials.language_switcher')
 
+            <div class="flex justify-center mt-6">
+                <a href="{{ url('/homepage') . '?locale=' . app()->getLocale() }}" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">{{ __('Go to Homepage') }}</a>
+            </div>
+
             <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                 {{ __('Welcome to our website!') }}
             </div>
