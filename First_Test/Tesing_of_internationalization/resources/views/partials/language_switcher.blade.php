6b8a8307835c5218823d743c3f514738986b8858
Piraat-Bram <bramkerste125@gmail.com>
2025-12-16 04:17:34 +0100

    Basic stuff

--- First_Test/Tesing_of_internationalization/resources/views/partials/language_switcher.blade.php
+++ First_Test/Tesing_of_internationalization/resources/views/partials/language_switcher.blade.php
@@ -1,11 +1,7 @@
 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-    @foreach($available_locales as $locale_name => $available_locale)
-        @if($available_locale === $current_locale)
-            <span class="ml-2 mr-2 text-gray-700">{{ $locale_name }}</span>
-        @else
-            <a class="ml-1 underline ml-2 mr-2" href="{{ request()->fullUrlWithQuery(['locale' => $available_locale]) }}">
-                <span>{{ $locale_name }}</span>
-            </a>
-        @endif
-    @endforeach
+    <select onchange="location.href = '{{ request()->fullUrlWithQuery(['locale' => '']) }}' + this.value;" class="px-3 py-2 border border-gray-300 rounded bg-white text-gray-700 hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500">
+        @foreach($available_locales as $locale_name => $available_locale)
+            <option value="{{ $available_locale }}" @selected($available_locale === $current_locale)>{{ $locale_name }}</option>
+        @endforeach
+    </select>
 </div>
